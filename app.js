b5715dce3f6ccd38832cff6c7a85ffda95e3c2e0
NoodleSomRequiem <semvanveen@outlook.com>
2025-03-28 17:41:53 +0100

    ena laatste update voor official launch, alleen nog maar schoonheids dingetjes

--- app.js
+++ app.js
@@ -3,15 +3,12 @@ const path = require('path');
 
 const app = express();
 
-// Serve static files from the current directory
 app.use(express.static(__dirname));
 
-// Define a route for the home page
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Define routes for other pages
 app.get('/aboutMe.html', (req, res) => {
     res.sendFile(path.join(__dirname, 'aboutMe.html'));
 });
@@ -20,7 +17,6 @@ app.get('/projects.html', (req, res) => {
     res.sendFile(path.join(__dirname, 'projects.html'));
 });
 
-// Set the port
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
